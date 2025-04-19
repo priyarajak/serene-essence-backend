b15e87f90f9ada24429b82bcc2b397bb066c753c
PRIYA RAJAK <72929067+priyarajak@users.noreply.github.com>
2025-04-19 20:59:10 +0530

    Update app.py

--- app.py
+++ app.py
@@ -209,4 +209,6 @@ def get_addresses(user_id):
 
 
 if __name__ == "__main__":
-    app.run(debug=True, port=5050)
+    import os
+    port = int(os.environ.get("PORT", 5050))  # Render provides PORT env
+    app.run(host="0.0.0.0", port=port)
